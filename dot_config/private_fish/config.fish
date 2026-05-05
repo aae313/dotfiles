0793708cdf9c775aafe520dc4b55b0c237070d1d
BaudilliardAlt <230780735+aae313@users.noreply.github.com>
2026-05-05 23:50:52 +0200

    Tue May 5 23:50:52 CEST 2026

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -12,7 +12,7 @@ if status is-interactive
     set fish_greeting ""
     set fish_key_bindings fish_vi_key_bindings
     starship init fish | source
-    zoxide init fish --cmd cd | source
+    zoxide init fish >~/.config/fish/conf.d/zoxide_init.fish
 end
 
 set -gx SUDO_EDITOR 'env -u NVIM_LISTEN_ADDRESS nvim'
@@ -22,12 +22,23 @@ set -gx FZF_DEFAULT_OPTS "\
 --cycle \
 --layout=reverse \
 --height 80% \
---color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
---color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
---color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
---color=selected-bg:#45475A \
---color=border:#6C7086,label:#CDD6F4"
-
+  --color=bg+:#283457 \
+  --color=bg:#000000 \
+  --color=border:#27a1b9 \
+  --color=fg:#c0caf5 \
+  --color=gutter:#16161e \
+  --color=header:#ff9e64 \
+  --color=hl+:#2ac3de \
+  --color=hl:#2ac3de \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#2ac3de \
+  --color=query:#c0caf5:regular \
+  --color=scrollbar:#27a1b9 \
+  --color=separator:#ff9e64 \
+  --color=spinner:#ff007c \
+"
 abbr -a cp cp -rv
 abbr -a rm rm -rvf
 abbr -a che chezmoi
